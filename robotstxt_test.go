2fc77e996edbc655b9377ee33762e91f31e2880e
itmayziii <tommymay37@gmail.com>
2019-05-04 23:32:57 -0400

    docs(Benchmarks and examples): Added benchmarks for parsing and regular methods, also added examples

--- robotstxt_test.go
+++ robotstxt_test.go
@@ -1,6 +1,7 @@
 package robotstxt_test
 
 import (
+	"fmt"
 	"github.com/itmayziii/robotstxt"
 	"github.com/stretchr/testify/assert"
 	"path/filepath"
@@ -219,6 +220,171 @@ func Test_NewFromFile(t *testing.T) {
 	assert.Equal(t, 0, robotsTxt.CrawlDelay("adsbot-google"))
 	assert.Equal(t, []string{"https://www.dumpsters.com/sitemap.xml", "https://www.dumpsters.com/sitemap-launch-index.xml"}, robotsTxt.Sitemaps())
 }
+
+/*
+ *********************************************** START BENCHMARKS ***********************************************
+ */
+
+func BenchmarkNew_with_parsing(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		robotsTxt, _ := robotstxt.New("", `
+# Robots.txt test file
+# 06/04/2018
+      # Indented comments are allowed
+
+User-agent : *
+Crawl-delay: 5
+Disallow: /cms/
+Disallow: /pricing/frontend
+Disallow: /pricing/admin/ # SPA application built into the site
+Disallow : *?s=lightbox
+Disallow: /se/en$
+Disallow:*/retail/*/frontend/*
+
+Allow: /be/fr_fr/retail/fr/
+
+# Multiple groups with all access
+User-agent: AdsBot-Google
+User-agent: AdsBot-Bing
+Allow: /
+
+# Multiple sitemaps
+Sitemap: https://www.dumpsters.com/sitemap.xml
+Sitemap: https://www.dumpsters.com/sitemap-launch-index.xml
+`)
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/cms/")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/cms")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/cms/pages")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/cms/pages?products=123")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/frontend")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/frontend-app")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/frontend/product")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/admin/product")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/admin")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing?s=lightbox")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing?s-lightbox&cart=full")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing?cart=full&s=lightbox")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/se/en")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/se/en/")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/se")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/se/en/fr")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/retail/online/frontend")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/store/retail/online/frontend")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/retail/online/frontend/pages?page=2")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/online/frontend/")
+	}
+}
+
+func BenchmarkNew_only_methods(b *testing.B) {
+	robotsTxt, _ := robotstxt.New("", `
+# Robots.txt test file
+# 06/04/2018
+      # Indented comments are allowed
+
+User-agent : *
+Crawl-delay: 5
+Disallow: /cms/
+Disallow: /pricing/frontend
+Disallow: /pricing/admin/ # SPA application built into the site
+Disallow : *?s=lightbox
+Disallow: /se/en$
+Disallow:*/retail/*/frontend/*
+
+Allow: /be/fr_fr/retail/fr/
+
+# Multiple groups with all access
+User-agent: AdsBot-Google
+User-agent: AdsBot-Bing
+Allow: /
+
+# Multiple sitemaps
+Sitemap: https://www.dumpsters.com/sitemap.xml
+Sitemap: https://www.dumpsters.com/sitemap-launch-index.xml
+`)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/cms/")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/cms")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/cms/pages")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/cms/pages?products=123")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/frontend")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/frontend-app")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/frontend/product")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/admin/product")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing/admin")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing?s=lightbox")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing?s-lightbox&cart=full")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/pricing?cart=full&s=lightbox")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/se/en")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/se/en/")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/se")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/se/en/fr")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/retail/online/frontend")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/store/retail/online/frontend")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/retail/online/frontend/pages?page=2")
+		_, _ = robotsTxt.CanCrawl("Bingbot", "/online/frontend/")
+	}
+}
+
+/*
+ *********************************************** END BENCHMARKS ***********************************************
+ */
+
+/*
+ *********************************************** START EXAMPLES ***********************************************
+ */
+
+func ExampleNew() {
+	robotsTxt, _ := robotstxt.New("", `
+# Robots.txt test file
+# 06/04/2018
+      # Indented comments are allowed
+
+User-agent : *
+Crawl-delay: 5
+Disallow: /cms/
+Disallow: /pricing/frontend
+Disallow: /pricing/admin/ # SPA application built into the site
+Disallow : *?s=lightbox
+Disallow: /se/en$
+Disallow:*/retail/*/frontend/*
+
+Allow: /be/fr_fr/retail/fr/
+
+# Multiple groups with all access
+User-agent: AdsBot-Google
+User-agent: AdsBot-Bing
+Allow: /
+
+# Multiple sitemaps
+Sitemap: https://www.dumpsters.com/sitemap.xml
+Sitemap: https://www.dumpsters.com/sitemap-launch-index.xml
+`)
+	canCrawl, err := robotsTxt.CanCrawl("googlebot", "/cms/pages")
+	fmt.Println(canCrawl)
+	fmt.Println(err)
+	// Output:
+	// false
+	// <nil>
+}
+
+func ExampleNewFromFile() {
+	filePath, err := filepath.Abs("./robots.txt")
+	robotsTxt, newFromFileErr := robotstxt.NewFromFile("https://www.dumpsters.com", filePath)
+	fmt.Println(newFromFileErr)
+	canCrawl, err := robotsTxt.CanCrawl("googlebot", "/cms/pages")
+	fmt.Println(canCrawl)
+	fmt.Println(err)
+	// Output:
+	// <nil>
+	// false
+	// <nil>
+}
+
+/*
+ *********************************************** END EXAMPLES ***********************************************
+ */
 
 type testUrl struct {
 	url       string
